8b8d00b21641007fa16d55c34c9fb5f190499e9a
CHOK <6206021611133@fitm.kmutnb.ac.th>
2022-01-26 10:28:05 +0700

    .

---
+++ Lecture3/map.js
@@ -0,0 +1,6 @@
+const finalParticipants = ["Taylor", "Donald", "Don", "Natasha", "Bobby"];
+
+const announcements = finalParticipants.map((member) => {
+  return member + "joined the contest";
+});
+console.log(announcements);
